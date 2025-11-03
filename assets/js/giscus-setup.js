a21df28d0a34f0a8936ce92e377baa63260d7263
WangVictor <xiaoyuwang1993@hotmail.com>
2025-11-03 15:48:12 +0000

    Deploying to gh-pages from @ WangVictor/wangvictor.github.io@00ece44705281a7ae4bb910fdf3c8310bf926d29 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "WangVictor/mywebsite",
+    "data-repo": "WangVictor/wangvictor.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
